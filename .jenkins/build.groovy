220053698fb348441779c0ec95302f4093275234
Bimali Dayananda <bud93411@users.noreply.github.com>
2017-10-13 22:13:47 +0530

    Update build.groovy

--- .jenkins/build.groovy
+++ .jenkins/build.groovy
@@ -29,7 +29,7 @@ node {
    // branchVersion = "${pomVersion}-${branchVersion}-SNAPSHOT"
 
     // set branch SNAPSHOT version in pom.xml
-    sh "mvn versions:set -DnewVersion=${branchVersion}"
+    sh "mvn versions:set -DnewVersion=${branchVersion}-SNAPSHOT"
     }
   }
 
