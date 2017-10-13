05ee33e41462322a8b15a6ba9adfe3cfcb97a787
bud93411 <bud93411@gmail.com>
2017-10-13 22:52:24 +0530

    jenkinsfile added

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM tomcat:8.0.38
 ARG PACKAGE_VERSION
 RUN echo "${PACKAGE_VERSION}" >> /usr/local/tomcat/webapps/version.txt
 RUN pwd
-ADD ../target/us.fetchr.sample-"${PACKAGE_VERSION}".war /usr/local/tomcat/webapps/project.war
+ADD ./target/us.fetchr.sample-"${PACKAGE_VERSION}".war /usr/local/tomcat/webapps/project.war
 COPY docker-entrypoint.sh /
 RUN chmod +x /docker-entrypoint.sh
 ENTRYPOINT ["/docker-entrypoint.sh"]
--- build.groovy
+++ build.groovy
@@ -48,7 +48,7 @@ node {
     withDockerServer([uri: ""]) {
       withDockerRegistry([credentialsId: '35a1f568-4a8e-4a2c-b415-728763bd8538', url: "https://0.0.0.0:4243/"]) {
         // we give the image the same version as the .war package
-        def image = docker.build("bud93411/jenkins-hello-world:${branchVersion}", "--build-arg PACKAGE_VERSION=${branchVersion} ./.docker/")
+        def image = docker.build("bud93411/jenkins-hello-world:${branchVersion}", "--build-arg PACKAGE_VERSION=${branchVersion} .")
         image.push()
       }   
     }
