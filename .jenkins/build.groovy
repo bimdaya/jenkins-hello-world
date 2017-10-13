4d5a20c70d460c7617b6d06576da1905b37afca2
Bimali Dayananda <bud93411@users.noreply.github.com>
2017-10-13 20:08:06 +0530

    bbbb

--- .jenkins/build.groovy
+++ .jenkins/build.groovy
@@ -19,7 +19,6 @@ node {
 
     // determine version in pom.xml
     def pomVersion = sh(script: 'mvn -q -Dexec.executable=\'echo\' -Dexec.args=\'${project.version}\' --non-recursive exec:exec', returnStdout: true).trim()
-
     echo "$pomVersion"
     // compute proper branch SNAPSHOT version
     //pomVersion = pomVersion.replaceAll(/-SNAPSHOT/, "") 
